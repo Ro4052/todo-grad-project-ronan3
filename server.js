9f338d42dc43e73b830ac4df4264d6af37481df6
Ronan Graham <Ro4052@users.noreply.github.com>
2018-08-30 14:14:30 +0100

    Rebuild with node server

---
+++ server.js
@@ -0,0 +1,8 @@
+var server = require("./server/server");
+var path = require("path");
+
+var port = process.env.PORT || 8080
+var dirPath = path.join(__dirname, "client/build");
+
+server(port, dirPath);
+console.log("Server running on port " + port);
---
+++ server/server.js
@@ -0,0 +1,88 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var underscore = require("underscore");
+
+module.exports = function(port, dirPath, middleware, callback) {
+    var app = express();
+
+    if (middleware) {
+        app.use(middleware);
+    }
+    app.use(express.static(dirPath));
+    app.use(bodyParser.json());
+
+    var latestId = 0;
+    var todos = [];
+
+    // Create
+    app.post("/api/todo", function(req, res) {
+        var todo = req.body;
+        todo.id = latestId.toString();
+        latestId++;
+        todos.push(todo);
+        res.status(201);
+        res.json({
+            newId: todo.id,
+            todos: todos
+        });
+    });
+
+    // Update
+    app.put("/api/todo/:id", function(req, res) {
+        var todo = getTodo(req.params.id);
+        if (todo) {
+            todo.title = req.body.title;
+            todo.isComplete = req.body.isComplete;
+            res.status(201);
+            res.json({
+                newId: todo.id,
+                todos: todos
+            });
+        } else {
+            res.sendStatus(404);
+        }
+    });
+
+    // Read
+    app.get("/api/todo", function(req, res) {
+        res.json(todos);
+    });
+
+    // Delete
+    app.delete("/api/todo/:id", function(req, res) {
+        var id = req.params.id;
+        var todo = getTodo(id);
+        if (todo) {
+            todos = todos.filter(function(otherTodo) {
+                return otherTodo !== todo;
+            });
+            res.status(200);
+            res.json(todos);
+        } else {
+            res.sendStatus(404);
+        }
+    });
+
+    function getTodo(id) {
+        return underscore.find(todos, function(todo) {
+            return todo.id === id
+        });
+    }
+
+    var server = app.listen(port, callback);
+
+    // We manually manage the connections to ensure that they're closed when calling close().
+    var connections = [];
+    server.on("connection", function(connection) {
+        connections.push(connection);
+    });
+
+    return {
+        close: function(callback) {
+            connections.forEach(function(connection) {
+                connection.destroy();
+            });
+            server.close(callback);
+        }
+    };
+};
