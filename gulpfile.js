ea0fbd8f838c8d15dfee07c3ad1cddf002ad4374
Ronan Graham <Ro4052@users.noreply.github.com>
2018-09-03 13:59:43 +0100

    Re-added server tests

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require("gulp");
+var mocha = require("gulp-mocha");
+var istanbul = require("gulp-istanbul");
+var spawn = require("child_process").spawn;
+
+var node;
+
+gulp.task("test:mocha", function () {
+  return gulp.src("server/**/*.js", { read: false })
+    .pipe(mocha({
+      reporter: "spec",
+    }));
+});
+
+gulp.task("test:istanbul:pre", function () {
+  return gulp.src("server/**/*.js")
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task("test:istanbul", ["test:istanbul:pre"], function () {
+  return gulp.src("server/**/*.js")
+    .pipe(mocha({ reporter: "spec" }))
+    .pipe(istanbul.writeReports({
+      dir: "./build_artifacts",
+      reporters: ["lcov", "text", "text-summary"],
+      reportOpts: { dir: "./build_artifacts" }
+    }))
+    .pipe(istanbul.enforceThresholds({ thresholds: { each: 95 } }));
+});
+
+gulp.task("serve", function () {
+  if (node) {
+    node.kill();
+  }
+  node = spawn("node", ["server.js"], { stdio: "inherit" });
+  node.on("close", function (code) {
+    if (code === 8) {
+      gulp.log("Error detected");
+    }
+  })
+});
+
+gulp.task("test", ["test:istanbul"]);
+gulp.task("default", ["serve"], function () {
+  gulp.watch("server/*", ["serve"]);
+});
+
+process.on("exit", function () {
+  if (node) {
+    node.kill();
+  }
+})
--- server/server.js
+++ server/server.js
@@ -2,79 +2,79 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var underscore = require('underscore');
 
-module.exports = function(port, dirPath, middleware, callback) {
-    var app = express();
+module.exports = function (port, dirPath, middleware, callback) {
+  var app = express();
 
-    if (middleware) {
-        app.use(middleware);
+  if (middleware) {
+    app.use(middleware);
+  }
+  app.use(express.static(dirPath));
+  app.use(bodyParser.json());
+
+  var latestId = 0;
+  var todos = [];
+
+  // Create
+  app.post('/api/todo', function (req, res) {
+    var todo = req.body;
+    todo.id = latestId.toString();
+    latestId++;
+    todos.push(todo);
+    res.status(201);
+    res.send(todo.id);
+  });
+
+  // Update
+  app.put('/api/todo/:id', function (req, res) {
+    var todo = getTodo(req.params.id);
+    if (todo) {
+      todo.title = req.body.title;
+      todo.isComplete = req.body.isComplete;
+      res.sendStatus(201);
+    } else {
+      res.sendStatus(404);
     }
-    app.use(express.static(dirPath));
-    app.use(bodyParser.json());
+  });
 
-    var latestId = 0;
-    var todos = [];
+  // Read
+  app.get('/api/todo', function (req, res) {
+    res.json(todos);
+  });
 
-    // Create
-    app.post('/api/todo', function(req, res) {
-        var todo = req.body;
-        todo.id = latestId.toString();
-        latestId++;
-        todos.push(todo);
-        res.status(201);
-        res.send(todo.id);
-    });
-
-    // Update
-    app.put('/api/todo/:id', function(req, res) {
-        var todo = getTodo(req.params.id);
-        if (todo) {
-            todo.title = req.body.title;
-            todo.isComplete = req.body.isComplete;
-            res.sendStatus(201);
-        } else {
-            res.sendStatus(404);
-        }
-    });
-
-    // Read
-    app.get('/api/todo', function(req, res) {
-        res.json(todos);
-    });
-
-    // Delete
-    app.delete('/api/todo/:id', function(req, res) {
-        var id = req.params.id;
-        var todo = getTodo(id);
-        if (todo) {
-            todos = todos.filter(function(otherTodo) {
-                return otherTodo !== todo;
-            });
-            res.sendStatus(200);
-        } else {
-            res.sendStatus(404);
-        }
-    });
-
-    function getTodo(id) {
-        return underscore.find(todos, function(todo) {
-            return todo.id === id
-        });
+  // Delete
+  app.delete('/api/todo/:id', function (req, res) {
+    var id = req.params.id;
+    var todo = getTodo(id);
+    if (todo) {
+      todos = todos.filter(function (otherTodo) {
+        return otherTodo !== todo;
+      });
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(404);
     }
+  });
 
-    var server = app.listen(port, callback);
-
-    // We manually manage the connections to ensure that they're closed when calling close().
-    var connections = [];
-    server.on('connection', function(connection) {
-        connections.push(connection);
+  function getTodo(id) {
+    return underscore.find(todos, function (todo) {
+      return todo.id === id
     });
+  }
 
-    return {
-        close: function(callback) {
-            connections.forEach(function(connection) {
-                connection.destroy();
-            });
-            server.close(callback);
-        }
-    };
+  var server = app.listen(port, callback);
+
+  // We manually manage the connections to ensure that they're closed when calling close().
+  var connections = [];
+  server.on('connection', function (connection) {
+    connections.push(connection);
+  });
+
+  return {
+    close: function (callback) {
+      connections.forEach(function (connection) {
+        connection.destroy();
+      });
+      server.close(callback);
+    }
+  };
 };
