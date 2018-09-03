26f3e1e24416cc403acda991f8643ebdca0a8eff
Ronan Graham <Ro4052@users.noreply.github.com>
2018-09-03 14:45:54 +0100

    Added another server test

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var server = require("./server/server");
-var path = require("path");
+var server = require('./server/server');
+var path = require('path');
 
 var port = process.env.PORT || 8080
-var dirPath = path.join(__dirname, "client/build");
+var dirPath = path.join(__dirname, 'client/build');
 
 server(port, dirPath);
-console.log("Server running on port " + port);
+console.log('Server running on port ' + port);
--- server/server.spec.js
+++ server/server.spec.js
@@ -1,12 +1,12 @@
-var server = require("./server");
-var request = require("request");
-var assert = require("chai").assert;
+var server = require('./server');
+var request = require('request');
+var assert = require('chai').assert;
 
 var testPort = 52684;
-var baseUrl = "http://localhost:" + testPort;
-var todoListUrl = baseUrl + "/api/todo";
+var baseUrl = 'http://localhost:' + testPort;
+var todoListUrl = baseUrl + '/api/todo';
 
-describe("server", function () {
+describe('server', function () {
   var serverInstance;
   beforeEach(function () {
     serverInstance = server(testPort, __dirname);
@@ -14,32 +14,32 @@ describe("server", function () {
   afterEach(function () {
     serverInstance.close();
   });
-  describe("get list of todos", function () {
-    it("responds with status code 200", function (done) {
+  describe('get list of todos', function () {
+    it('responds with status code 200', function (done) {
       request(todoListUrl, function (error, response) {
         assert.equal(response.statusCode, 200);
         done();
       });
     });
-    it("responds with a body encoded as JSON in UTF-8", function (done) {
+    it('responds with a body encoded as JSON in UTF-8', function (done) {
       request(todoListUrl, function (error, response) {
-        assert.equal(response.headers["content-type"], "application/json; charset=utf-8");
+        assert.equal(response.headers['content-type'], 'application/json; charset=utf-8');
         done();
       });
     });
-    it("responds with a body that is a JSON empty array", function (done) {
+    it('responds with a body that is a JSON empty array', function (done) {
       request(todoListUrl, function (error, response, body) {
-        assert.equal(body, "[]");
+        assert.equal(body, '[]');
         done();
       });
     });
   });
-  describe("create a new todo", function () {
-    it("responds with status code 201", function (done) {
+  describe('create a new todo', function () {
+    it('responds with status code 201', function (done) {
       request.post({
         url: todoListUrl,
         json: {
-          title: "This is a TODO item",
+          title: 'This is a TODO item',
           isComplete: false
         }
       }, function (error, response) {
@@ -47,55 +47,67 @@ describe("server", function () {
         done();
       });
     });
-    it("inserts the todo at the end of the list of todos", function (done) {
+    it('responds with id of new todo', function (done) {
       request.post({
         url: todoListUrl,
         json: {
-          title: "This is a TODO item",
+          title: 'This is a TODO item',
+          isComplete: false
+        }
+      }, function (error, response, body) {
+        assert.equal(body, 0);
+        done();
+      });
+    });
+    it('inserts the todo at the end of the list of todos', function (done) {
+      request.post({
+        url: todoListUrl,
+        json: {
+          title: 'This is a TODO item',
           isComplete: false
         }
       }, function () {
         request.get(todoListUrl, function (error, response, body) {
           assert.deepEqual(JSON.parse(body), [{
-            title: "This is a TODO item",
+            title: 'This is a TODO item',
             isComplete: false,
-            id: "0"
+            id: '0'
           }]);
           done();
         });
       });
     });
   });
-  describe("delete a todo", function () {
-    it("responds with status code 404 if there is no such item", function (done) {
-      request.del(todoListUrl + "/0", function (error, response) {
+  describe('delete a todo', function () {
+    it('responds with status code 404 if there is no such item', function (done) {
+      request.del(todoListUrl + '/0', function (error, response) {
         assert.equal(response.statusCode, 404);
         done();
       });
     });
-    it("responds with status code 200", function (done) {
+    it('responds with status code 200', function (done) {
       request.post({
         url: todoListUrl,
         json: {
-          title: "This is a TODO item",
+          title: 'This is a TODO item',
           isComplete: false
         }
       }, function () {
-        request.del(todoListUrl + "/0", function (error, response) {
+        request.del(todoListUrl + '/0', function (error, response) {
           assert.equal(response.statusCode, 200);
           done();
         });
       });
     });
-    it("removes the item from the list of todos", function (done) {
+    it('removes the item from the list of todos', function (done) {
       request.post({
         url: todoListUrl,
         json: {
-          title: "This is a TODO item",
+          title: 'This is a TODO item',
           isComplete: false
         }
       }, function () {
-        request.del(todoListUrl + "/0", function () {
+        request.del(todoListUrl + '/0', function () {
           request.get(todoListUrl, function (error, response, body) {
             assert.deepEqual(JSON.parse(body), []);
             done();
@@ -104,59 +116,59 @@ describe("server", function () {
       });
     });
   });
-  describe("update a todo", function () {
-    it("responds with status code 404 if there is no such item", function (done) {
-      request.put(todoListUrl + "/0", function (error, response) {
+  describe('update a todo', function () {
+    it('responds with status code 404 if there is no such item', function (done) {
+      request.put(todoListUrl + '/0', function (error, response) {
         assert.equal(response.statusCode, 404);
         done();
       });
     });
-    it("changes the title of the todo item", function (done) {
+    it('changes the title of the todo item', function (done) {
       request.post({
         url: todoListUrl,
         json: {
-          title: "This is a TODO item",
+          title: 'This is a TODO item',
           isComplete: false
         }
       }, function (error, response) {
         request.put({
-          url: todoListUrl + "/0",
+          url: todoListUrl + '/0',
           json: {
-            title: "This is a new name",
+            title: 'This is a new name',
             isComplete: false
           }
         }, function (error, response) {
           request.get(todoListUrl, function (error, response, body) {
             assert.deepEqual(JSON.parse(body), [{
-              title: "This is a new name",
+              title: 'This is a new name',
               isComplete: false,
-              id: "0"
+              id: '0'
             }]);
             done();
           });
         });
       });
     });
-    it("changes the isComplete of the todo item", function (done) {
+    it('changes the isComplete of the todo item', function (done) {
       request.post({
         url: todoListUrl,
         json: {
-          title: "This is a TODO item",
+          title: 'This is a TODO item',
           isComplete: false
         }
       }, function (error, response) {
         request.put({
-          url: todoListUrl + "/0",
+          url: todoListUrl + '/0',
           json: {
-            title: "This is a TODO item",
+            title: 'This is a TODO item',
             isComplete: true
           }
         }, function (error, response) {
           request.get(todoListUrl, function (error, response, body) {
             assert.deepEqual(JSON.parse(body), [{
-              title: "This is a TODO item",
+              title: 'This is a TODO item',
               isComplete: true,
-              id: "0"
+              id: '0'
             }]);
             done();
           });
